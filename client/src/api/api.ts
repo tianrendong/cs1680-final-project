2c99346b73e20285a7bd62a3f6846b29410f840e
Jenny Yu <jennyyu212@outlook.com>
2022-12-06 12:45:46 -0500

    feat: playing sound (noise...)

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -30,7 +30,7 @@ export async function sayHello(userId: string, addToMsgList: (m: string) => void
                         // console.log(buf.buffer)
                         const audioCtx = new AudioContext();
                         const source = new AudioBufferSourceNode(audioCtx)
-                        source.buffer = await audioCtx.decodeAudioData(buf.buffer)
+                        source.buffer = await audioCtx.decodeAudioData(withWaveHeader(buf.buffer, 2, 14400))
                         source.connect(audioCtx.destination)
                         source.start()
                   }
@@ -107,3 +107,50 @@ export async function broadcastMusicFile(userId: string, music: File) {
 //       });
 //       return new Promise((resolve) => { resolve(source) })
 // }
+
+const withWaveHeader = (data: any, numberOfChannels: any, sampleRate: any) => {
+      const header = new ArrayBuffer(44);
+
+      const d = new DataView(header);
+
+      d.setUint8(0, "R".charCodeAt(0));
+      d.setUint8(1, "I".charCodeAt(0));
+      d.setUint8(2, "F".charCodeAt(0));
+      d.setUint8(3, "F".charCodeAt(0));
+
+      d.setUint32(4, data.byteLength / 2 + 44, true);
+
+      d.setUint8(8, "W".charCodeAt(0));
+      d.setUint8(9, "A".charCodeAt(0));
+      d.setUint8(10, "V".charCodeAt(0));
+      d.setUint8(11, "E".charCodeAt(0));
+      d.setUint8(12, "f".charCodeAt(0));
+      d.setUint8(13, "m".charCodeAt(0));
+      d.setUint8(14, "t".charCodeAt(0));
+      d.setUint8(15, " ".charCodeAt(0));
+
+      d.setUint32(16, 16, true);
+      d.setUint16(20, 1, true);
+      d.setUint16(22, numberOfChannels, true);
+      d.setUint32(24, sampleRate, true);
+      d.setUint32(28, sampleRate * 1 * 2);
+      d.setUint16(32, numberOfChannels * 2);
+      d.setUint16(34, 16, true);
+
+      d.setUint8(36, "d".charCodeAt(0));
+      d.setUint8(37, "a".charCodeAt(0));
+      d.setUint8(38, "t".charCodeAt(0));
+      d.setUint8(39, "a".charCodeAt(0));
+      d.setUint32(40, data.byteLength, true);
+
+      return concat(header, data);
+};
+
+const concat = (buffer1: any, buffer2: any) => {
+      const tmp = new Uint8Array(buffer1.byteLength + buffer2.byteLength);
+
+      tmp.set(new Uint8Array(buffer1), 0);
+      tmp.set(new Uint8Array(buffer2), buffer1.byteLength);
+
+      return tmp.buffer;
+};
