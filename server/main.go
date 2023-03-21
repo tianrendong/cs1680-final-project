8cb7ca70985f5aea5d09055035adfa1ac96d9cc8
Tianren Dong <tianrendong.dtr@gmail.com>
2023-03-21 14:07:24 -0400

    fix: use env port

--- server/main.go
+++ server/main.go
@@ -2,17 +2,22 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net"
+	"os"
 
 	pb "github.com/jennyyu212/cs1680-final-project/pb"
 	"github.com/jennyyu212/cs1680-final-project/pkg/snowcast"
+	"github.com/joho/godotenv"
 	"google.golang.org/grpc"
 )
 
 func main() {
 	flag.Parse()
-	listener, err := net.Listen("tcp", ":8000")
+	godotenv.Load()
+	port := os.Getenv("PORT")
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%s", port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
